dd4d6b1bb46adc96c4cde02fc46e3cc289d11e48
Sounplace <team@sounplace.com>
2026-03-11 19:29:19 -0500

    fix vault route

--- app/routes/vault/_index.ts
+++ app/routes/vault/_index.ts
@@ -1,17 +1,3 @@
 export async function loader() {
-  return new Response(
-    `
-    <html>
-      <body style="font-family: sans-serif; padding:40px">
-        <h1>Soundplace Vault Connected</h1>
-        <p>The Shopify App Proxy is working.</p>
-      </body>
-    </html>
-    `,
-    {
-      headers: {
-        "Content-Type": "text/html",
-      },
-    }
-  );
+  return new Response("Vault working");
 }
